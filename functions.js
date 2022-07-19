c96ce269112b1d1ad4bf258dd71f3a551853defb
rpi-esp32 <rpi.esp32@gmail.com>
2022-07-19 16:02:51 +0800

    which in function, which in initiation

--- functions.js
+++ functions.js
@@ -1,8 +1,21 @@
+let onDataSubscribers = [];
+let sendDataSubscribers = [];
+
 
 function onData() {
     console.info("data received from Websocket/BLE/MQTT/LORA");
+    // do onDataSubscriber(onDataSubscribers)
 }
 
 function sendData() {
     console.info("send message out via WS/BLE/mqtt/lora");
+    // do sendDataSubscriber(sendDataSubscribers)
+}
+
+function subscribeOnData(func) {
+    onDataSubscribers.append(func);
+}
+
+function subscribeSendData(func) {
+    sendDataSubscribers.append(func);
 }
--- initiation.js
+++ initiation.js
@@ -1,9 +1,3 @@
-let receive = ["ws", "mqtt", "ble", "lora",];   // modify as needed
-let send = ["ws", "mqtt", "ble", "lora",];      // modify as needed
-let data = ["type", 1, "label", "value", "sender", "receiver", "medium", "topic", "time",];
-
-url = "ws://127.0.0.1";
-
 
 
 function setupWS() {
