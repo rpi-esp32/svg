68428618a521a5423c50c59c6979c666879ef048
rpi-esp32 <rpi.esp32@gmail.com>
2022-07-19 16:03:16 +0800

    juggling

--- functions.js
+++ functions.js
@@ -1,5 +1,14 @@
+let receiveMedia = ["ws", "mqtt", "ble", "lora",];   // modify as needed
+let sendMedia = ["ws", "mqtt", "ble", "lora",];      // modify as needed
+let data = ["type", 1, "label", "value", "sender", "receiver", "medium", "topic", "time",];
+
+url = "ws://127.0.0.1";
+
+
+
+
 let onDataSubscribers = [];
-let sendDataSubscribers = [];
+// let sendDataSubscribers = [];
 
 
 function onData() {
@@ -7,15 +16,12 @@ function onData() {
     // do onDataSubscriber(onDataSubscribers)
 }
 
-function sendData() {
+function sendData(msg) {
     console.info("send message out via WS/BLE/mqtt/lora");
-    // do sendDataSubscriber(sendDataSubscribers)
+    for (sendMethod in sendMethods) { sendMethod.send(msg); }
 }
 
 function subscribeOnData(func) {
     onDataSubscribers.append(func);
 }
 
-function subscribeSendData(func) {
-    sendDataSubscribers.append(func);
-}
